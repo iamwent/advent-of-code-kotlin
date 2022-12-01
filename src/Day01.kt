24b5198e443c2bb0f63118043a904d78463c8cb2
iamwent <ilovewent@gmail.com>
2022-12-01 19:55:07 +0800

    Day 1: Calorie Counting

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(calories: List<Int>): Int {
+        return calories.max()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(calories: List<Int>): Int {
+        return calories.sortedDescending().take(3).sum()
+    }
+
+    fun readCalories(name: String): List<Int> {
+        return readText(name).split("\n\n")
+            .map { calories -> calories.split("\n").sumOf { it.toInt() } }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testCalories = readCalories("Day01_test")
+    check(part1(testCalories) == 24000)
+    check(part2(testCalories) == 45000)
 
-    val input = readInput("Day01")
+    val input = readCalories("Day01")
     println(part1(input))
     println(part2(input))
 }
